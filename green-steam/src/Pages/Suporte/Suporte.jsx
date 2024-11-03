cb51bae67a9067a0847f19c914c719b240dd6e7e
romulo.louremco <romulo.lourenco1@gmail.com>
2024-11-03 17:35:59 -0300

    textos do suporte

--- green-steam/src/Pages/Suporte/Suporte.jsx
+++ green-steam/src/Pages/Suporte/Suporte.jsx
@@ -11,13 +11,18 @@ export function Suporte(){
 
         <div>
         <h1>Suporte</h1>
-        <h3>Queremos garantir que sua experiência de compra seja rápida, segura e fácil. Caso precise de assistência, nossa equipe está pronta para ajudar em todas as etapas.</h3>
+        <h3>Queremos garantir que sua experiência de compra seja rápida, segura e fácil.
+          <br />Caso precise de assistência, nossa equipe está pronta para ajudar em todas as etapas.</h3>
 
         <h3>Dúvidas sobre pedidos: Acompanhe o status do seu pedido, altere informações de entrega e saiba mais sobre nossa política de envio.</h3>
 
+        <h3>Pagamentos e reembolsos: Encontre respostas sobre formas de pagamento, segurança, e os procedimentos para reembolsos.</h3>
+
+        <h3>Problemas com produtos: Recebeu um produto com defeito ou diferente do esperado? Confira nossas instruções para troca e devolução.</h3>
+        
         <h2>informaçoes com todos os produtos e buttons</h2>
         </div>
-
+        
         <Footer />
         </>   
     </>
