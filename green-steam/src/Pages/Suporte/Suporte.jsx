b75f2cd6828dddbf09bc0165d8f6ccd9785ff317
romulo.louremco <romulo.lourenco1@gmail.com>
2024-11-03 16:28:47 -0300

    suporte primeira versão

--- green-steam/src/Pages/Suporte/Suporte.jsx
+++ green-steam/src/Pages/Suporte/Suporte.jsx
@@ -11,9 +11,11 @@ export function Suporte(){
 
         <div>
         <h1>Suporte</h1>
-        <p>Luismod, ligula non ultricitac hacilisi. Nulla facilisi.</p>
+        <h3>Queremos garantir que sua experiência de compra seja rápida, segura e fácil. Caso precise de assistência, nossa equipe está pronta para ajudar em todas as etapas.</h3>
 
-        <h1>informaçoes com todos os produtos e buttons</h1>
+        <h3>Dúvidas sobre pedidos: Acompanhe o status do seu pedido, altere informações de entrega e saiba mais sobre nossa política de envio.</h3>
+
+        <h2>informaçoes com todos os produtos e buttons</h2>
         </div>
 
         <Footer />
