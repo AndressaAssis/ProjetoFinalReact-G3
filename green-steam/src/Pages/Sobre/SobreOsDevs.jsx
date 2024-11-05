dcae914efbc73e3ab9cb58b7d9caf82c890bd5fc
Tavaresjg <joaogabrieltavaresiqueira27@gmail.com>
2024-11-05 17:02:18 -0300

    comit alteracoes css nas pags, sobre, login, cadastro e carrinho

--- green-steam/src/Pages/Sobre/SobreOsDevs.jsx
+++ green-steam/src/Pages/Sobre/SobreOsDevs.jsx
@@ -1,64 +1,98 @@
 import React from "react";
-import tavares from "../../assets/tavares.png";
 
 import styles from "./Sobre.module.css";
 
 export function SobreOsDevs() {
+  return (
+    <>
+      <div className={styles.containerDev}>
+        <h1>Desenvolvedores do Projeto</h1>
+        <div className={styles.listaDesenvolvedores}>
+          {/* Desenvolvedor 1 */}
+          <div className={styles.desenvolvedor}>
+            <img
+              src="https://media.licdn.com/dms/image/v2/D4D03AQGD_tsu95q1ZQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1710159400215?e=1735776000&v=beta&t=CrZZp9yTob0DbLB3zxwezwaSsfhyKSIKjLEm0HRKC8o"
+              alt="Desenvolvedor 1"
+              className={styles.foto}
+            />
+            <h2>Andressa Jandre</h2>
+            <p>
+              <strong>Qualidades e Expertises:</strong>Cria animações e
+              transições que tornam a navegação pelos jogos mais envolvente.
+            </p>
+          </div>
 
-    return(
-        <>
+          {/* Desenvolvedor 2 */}
+          <div className={styles.desenvolvedor}>
+            <img
+              src="https://avatars.githubusercontent.com/u/177683424?v=4&size=64https://avatars.githubusercontent.com/u/177683424?v=4"
+              alt="Desenvolvedor 2"
+              className={styles.foto}
+            />
+            <h2>Desenvolvedor 2</h2>
+            <p>
+              <strong>Qualidades e Expertises:</strong> Otimiza componentes para
+              garantir uma experiência de compra rápida e fluida.
+            </p>
+          </div>
 
-        <div className={styles.containerDev}>
-            <h1>Desenvolvedores do Projeto</h1>
-            <div className={styles.listaDesenvolvedores}>
+          {/* Desenvolvedor 3 */}
+          <div className={styles.desenvolvedor}>
+            <img
+              src="https://media.licdn.com/dms/image/v2/C4D03AQFZNmMueWQJRA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1600806669278?e=1735776000&v=beta&t=lz6-Ftt9YzNvJxMidH4CIOQs9A-AaYs_yctbpU4v37Q"
+              alt="Desenvolvedor 3"
+              className={styles.foto}
+            />
+            <h2>Luiz Fernando</h2>
+            <p>
+              <strong>Qualidades e Expertises:</strong> Garante que a página de
+              games seja inclusiva para todos os jogadores.
+            </p>
+          </div>
 
-                {/* Desenvolvedor 1 */}
-                <div className={styles.desenvolvedor}>
-                    <img src="https://media.licdn.com/dms/image/v2/D4D03AQGD_tsu95q1ZQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1710159400215?e=1735776000&v=beta&t=CrZZp9yTob0DbLB3zxwezwaSsfhyKSIKjLEm0HRKC8o" alt="Desenvolvedor 1" className={styles.foto} />
-                    <h2>Andressa Jandre</h2>
-                    <p><strong>Qualidades e Expertises:</strong>Cria animações e transições que tornam a navegação pelos jogos mais envolvente.</p>
-                </div>
+          {/* Desenvolvedor 4 */}
+          <div className={styles.desenvolvedor}>
+            <img
+              src="https://media.licdn.com/dms/image/v2/C4E03AQEgfuuADyrGSw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1562018294124?e=1735776000&v=beta&t=lhOvo1DmVSE5ehVwwdNNCTArZZpuKNgzvh3s4ShZ69U"
+              alt="Desenvolvedor 3"
+              className={styles.foto}
+            />
+            <h2>Milton Rodrigues</h2>
+            <p>
+              <strong>Qualidades e Expertises:</strong> Assegura uma
+              visualização impecável da loja de games em qualquer dispositivo.
+            </p>
+          </div>
 
-                {/* Desenvolvedor 2 */}
-                <div className={styles.desenvolvedor}>
-                    <img src={tavares} alt="Desenvolvedor 2" className={styles.foto} />
-                    <h2>Desenvolvedor 2</h2>
-                    <p><strong>Qualidades e Expertises:</strong> Otimiza componentes para garantir uma experiência de compra rápida e fluida.
+          {/* Desenvolvedor 5 */}
+          <div className={styles.desenvolvedor}>
+            <img
+              src="https://media.licdn.com/dms/image/v2/D4D03AQGbcv5d4j7PhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726230239282?e=1735776000&v=beta&t=B2fIkt_D87f75ovTyK7muMVpPUeGk7KjNiHtp-W-g1Y"
+              alt="Desenvolvedor 2"
+              className={styles.foto}
+            />
+            <h2>Tiago Ventura</h2>
+            <p>
+              <strong>Qualidades e Expertises:</strong> Cria componentes
+              reutilizáveis que facilitam o lançamento de novos jogos na loja.
+            </p>
+          </div>
 
-                        </p>
-                </div>
-
-                {/* Desenvolvedor 3 */}
-                <div className={styles.desenvolvedor}>
-                    <img src="https://media.licdn.com/dms/image/v2/C4D03AQFZNmMueWQJRA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1600806669278?e=1735776000&v=beta&t=lz6-Ftt9YzNvJxMidH4CIOQs9A-AaYs_yctbpU4v37Q" alt="Desenvolvedor 3" className={styles.foto} />
-                    <h2>Luiz Fernando</h2>
-                    <p><strong>Qualidades e Expertises:</strong> Garante que a página de games seja inclusiva para todos os jogadores.</p>
-                </div>
-
-                {/* Desenvolvedor 4 */}
-                <div className={styles.desenvolvedor}>
-                    <img src="https://media.licdn.com/dms/image/v2/C4E03AQEgfuuADyrGSw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1562018294124?e=1735776000&v=beta&t=lhOvo1DmVSE5ehVwwdNNCTArZZpuKNgzvh3s4ShZ69U" alt="Desenvolvedor 3" className={styles.foto} />
-                    <h2>Milton Rodrigues</h2>
-                    <p><strong>Qualidades e Expertises:</strong> Assegura uma visualização impecável da loja de games em qualquer dispositivo.</p>
-                </div>
-
-                {/* Desenvolvedor 5 */}
-                <div className={styles.desenvolvedor}>
-                    <img src="https://media.licdn.com/dms/image/v2/D4D03AQGbcv5d4j7PhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726230239282?e=1735776000&v=beta&t=B2fIkt_D87f75ovTyK7muMVpPUeGk7KjNiHtp-W-g1Y" alt="Desenvolvedor 2" className={styles.foto} />
-                    <h2>Tiago Ventura</h2>
-                    <p><strong>Qualidades e Expertises:</strong> Cria componentes reutilizáveis que facilitam o lançamento de novos jogos na loja.</p>
-                </div>
-                
-                {/* Desenvolvedor 6 */}
-                <div className={styles.desenvolvedor}>
-                    <img src="https://avatars.githubusercontent.com/u/177690905?v=4" alt="Desenvolvedor 1" className={styles.foto} />
-                    <h2>Romulo Lourenço</h2>
-                    <p><strong>Qualidades e Expertises:</strong>Testa rigorosamente cada componente para uma navegação sem falhas na loja de games. </p>
-                </div>
-
-            </div>
+          {/* Desenvolvedor 6 */}
+          <div className={styles.desenvolvedor}>
+            <img
+              src="https://avatars.githubusercontent.com/u/177690905?v=4"
+              alt="Desenvolvedor 1"
+              className={styles.foto}
+            />
+            <h2>Romulo Lourenço</h2>
+            <p>
+              <strong>Qualidades e Expertises:</strong>Testa rigorosamente cada
+              componente para uma navegação sem falhas na loja de games.{" "}
+            </p>
+          </div>
         </div>
-        
-        </>
-    )
+      </div>
+    </>
+  );
 }
