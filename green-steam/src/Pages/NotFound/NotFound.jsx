b2b1230570e0d478481acd3152b145b3974a760c
Tavaresjg <joaogabrieltavaresiqueira27@gmail.com>
2024-11-05 21:43:04 -0300

    atualizando css das paginas

--- green-steam/src/Pages/NotFound/NotFound.jsx
+++ green-steam/src/Pages/NotFound/NotFound.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import React from "react";
+import styles from "./notFound.module.css";
 
 export function NotFoundPage() {
     const navigate = useNavigate();
@@ -6,11 +8,18 @@ export function NotFoundPage() {
 
     return (
         <>
-            <h1>Página não encontrada</h1>
-            <h1>Error 404: Not Found</h1>
-            <p>Desculpe, a página que você está procurando não existe.</p>
+            <div className={styles.txt}>
+                <h1>Página não encontrada</h1>
+                <h1>Error 404: Not Found</h1>
+                <p>Desculpe, a página que você está procurando não existe.</p>
+            </div>
 
-            <button onClick={handleNavigation}>Voltar para a Página Inicial</button>
+            {/* Centralizando o botão com a imagem */}
+            <div className={styles.botao}>
+                <button onClick={handleNavigation}>
+                    <img src="https://i.pinimg.com/736x/9b/a5/57/9ba557f03fe0bf85d722e2560c1ab665.jpg" alt="Voltar" className={styles.imgBotao} />
+                </button>
+            </div>
         </>
     );
 }
