d4a139c0fca62c4b34a78714ec4a38d4f7f3d5e2
Tronin1000 <tiagoventuratec@gmail.com>
2024-11-05 22:06:05 -0300

    erro api

--- green-steam/src/Services/Api.js
+++ green-steam/src/Services/Api.js
@@ -1,16 +1,11 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-export const api = axios.create({
-  baseURL: 'http://localhost:8080',
-=======
 const api = axios.create({
-  baseURL: 'http://localhost:8081',
->>>>>>> b2b1230570e0d478481acd3152b145b3974a760c
+  baseURL: 'http://localhost:8081', 
   headers: {
     'Content-Type': 'application/json',
   },
-});
+})
 
 export const adicionarCliente = (dadosCliente) => {
   return api.post('/cliente', dadosCliente)  
