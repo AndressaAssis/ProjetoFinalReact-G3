37f597242948d7cc923c03d58a9fde8d002f1c57
Tronin1000 <tiagoventuratec@gmail.com>
2024-11-05 22:09:06 -0300

    corrigido

--- green-steam/src/Services/Api.js
+++ green-steam/src/Services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8081', 
-  headers: {
+export const api = axios.create({
+  baseURL: 'http://localhost:8080', 
+  headers: { 
     'Content-Type': 'application/json',
   },
 })
